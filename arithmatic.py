7ea8282757126ae01e146257d92703faf300b2d6
Akash Kumar <akdev63063@gmail.com>
2026-06-15 16:58:13 +0530

    Arithmatics all operations

---
+++ arithmatic.py
@@ -0,0 +1,11 @@
+# Write a program to perform different Arithmetic Operations on numbers in Python
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+
+print("Addition : ", a + b )
+print("Subtraction : ", a - b )
+print("Multiplication : ", a * b )
+print("Devidation : ", a / b )
+print("Modulo : ", a % b )
+print("Floor Devision : ", a // b )
+print("Exponent  : ", a ** b )
