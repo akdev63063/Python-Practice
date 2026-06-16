60268d637307c4fc37f10cd8825bd53af1f4c081
Akash Kumar <akdev63063@gmail.com>
2026-06-16 10:08:05 +0530

    list append,remove

---
+++ list_method.py
@@ -0,0 +1,11 @@
+# Write a program to create, append, and remove lists in python. 
+l1 = []
+l1.append(10)
+l1.append(20)
+l1.append(30)
+l1.append(40)
+l1.append(50)
+l1.append(60)
+print(l1)
+l1.remove(60)
+print(l1)
