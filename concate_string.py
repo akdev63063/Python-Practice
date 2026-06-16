9014443107b9ba55aee18724dd7995086d6d9f43
Akash Kumar <akdev63063@gmail.com>
2026-06-16 09:45:25 +0530

    Current time and Date print

---
+++ concate_string.py
@@ -0,0 +1,15 @@
+"""Write a program to create, concatenate and print a string and
+accessing sub-string from a given string"""
+
+s1 = input("Enter First String : ")
+s2 = input("Enter Secend String : ")
+
+print("First String is : ",s1)
+print("First String is : ",s2)
+
+# Concatination
+print("Concatenate: ", s1 + s2)
+# Substrug 
+print(s1[1:3] + s2[1:3])
+
+
