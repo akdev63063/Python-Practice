e0c6486121a0af2f7978069f0624789fee14890a
Akash Kumar <akdev63063@gmail.com>
2026-06-16 10:26:53 +0530

    Tuple and methods

---
+++ tuple.py
@@ -0,0 +1,9 @@
+# 6. Write a program to demonstrate working with tuples in python.
+t1 = ()
+print(t1)
+print(type(t1))
+t1= t1 + (1,2,3,4)
+print(t1)
+# methods
+print(t1.count(1))
+print(t1.index(4))
