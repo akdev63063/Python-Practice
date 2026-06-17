ea15624327aa616ffdc9ad7803ecd3d242b59ca9
Akash Kumar <akdev63063@gmail.com>
2026-06-17 20:01:22 +0530

    Dictionary

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+# 7. Write a program to demonstrate working with dictionaries in python.
+
+dic1 ={1 : "Akash",
+        2: "Ajay",
+        3: "Vijay",
+        4: "Ravi",
+        5: "Shani"
+      }
+
+print(dic1[1])
+print(dic1[2])
+print(dic1[3])
+print(dic1[4])
+print(dic1) #print dictionary 
+# 1. get() Key ki value return karta hai.
+print(dic1.get(1))
+# 2. keys() Sabhi keys return karta hai.
+print(dic1.keys())
+# 2. keys() Sabhi keys return karta hai.
+print(dic1.values())
+# 4. items() Key-value pairs return karta hai.
+print(dic1.items())
+
+
+
+
+
+
+
+
