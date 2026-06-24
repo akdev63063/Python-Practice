4da93d5eeed88f92d3f262e7caf76d8bab393ffc
Akash Kumar <akdev63063@gmail.com>
2026-06-24 12:25:30 +0530

    Fabonacci series

--- fabonacci.py
+++ fabonacci.py
@@ -2,3 +2,15 @@
 14. Write a python program to define a module to find Fibonacci
 Numbers and import the module to another program
 """
+def fab(n):
+    a , b = 0 , 1
+    while b < a :
+        print(b , end=' ')
+        a , b = b , a+b
+        print()
+
+def fab2(n):
+    a , b = 0 , 1
+    while b < a :
+        print(b , end=' ')
+        a , b = b , a+b
